9cb8e935d483805b21fa856084032018dfb4d3e6
MichalPlatko <miso.platko@gmail.com>
2021-06-21 21:35:30 +0200

    init

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles/Introduction.scss";
+import {Avatar} from "@material-ui/core";
+
+function Introduction() {
+    return (
+        <div className="intro">
+        <Avatar style={{width:150,height:150}}/>
+        </div>
+    )
+}
+
+export default Introduction;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./styles/Navbar.scss";
+import {AppBar,Button,makeStyles} from "@material-ui/core";
+
+const useStyles=makeStyles(()=>({
+    root:{
+        backgroundColor:"#212121",
+    }
+}))
+
+function Navbar() {
+    const classes=useStyles();
+    return (
+        <AppBar position="static" classes={{root:classes.root}}>
+        <div className="navbar">
+        <div>Logo</div>
+        <div className="btns">
+        <Button variant="text" classes={{root:classes.button}} href="/">Home</Button>
+        <Button variant="text" classes={{root:classes.button}} href="about">About</Button>
+        <Button variant="text" classes={{root:classes.button}} href="work">Work</Button>
+        <Button variant="text" classes={{root:classes.button}} href="contact">Contact</Button>
+        </div>
+        </div>
+        </AppBar>
+    )
+}
+
+export default Navbar;
