2fda4aefe469d66f1a8cd538d316e8d7bb773223
MichalPlatko <miso.platko@gmail.com>
2021-06-25 13:04:47 +0200

    intro added desc

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./styles/Introduction.scss";
-import {Avatar} from "@material-ui/core";
+import {Avatar,Typography,Button} from "@material-ui/core";
 
 function Introduction() {
     return (
         <div className="intro">
         <Avatar style={{width:150,height:150}}/>
+        <div className="description">
+        <Typography variant="h4">Hello,</Typography>
+        <Typography variant="body1" gutterBottom>
+        My name is Michal Platko.<br/>
+        I am a web developer located in Presov,Slovakia.<br/>
+        And I am React Enthusiast!<br/>
+        </Typography>
+        <Button variant="contained" color="primary">View my work ↓</Button>
+        </div>
         </div>
     )
 }
