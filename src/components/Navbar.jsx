d5d6f3c66f0722d94f0960d789afb5430d576bbf
MichalPlatko <miso.platko@gmail.com>
2021-06-27 18:34:06 +0200

    added scrollIntoView

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import "./styles/Navbar.scss";
 import {AppBar,Button} from "@material-ui/core";
 
 function Navbar() {
+    const scrollTo=(element)=>{
+        element.scrollIntoView({behavior:"smooth",block:"start",inline:"start"});
+    }
     return (
         <AppBar position="sticky">
         <div className="navbar">
         <div>Logo</div>
         <div className="btns">
-        <Button variant="text">Home</Button>
-        <Button variant="text">About</Button>
-        <Button variant="text">Work</Button>
-        <Button variant="text">Contact</Button>
+        <Button variant="text" onClick={()=>{scrollTo(document.getElementById("intro"))}}>About</Button>
+        <Button variant="text" onClick={()=>{scrollTo(document.getElementById("projects"))}}>Work</Button>
+        <Button variant="text" onClick={()=>{scrollTo(document.getElementById("contacts"))}}>Contact</Button>
         </div>
         </div>
         </AppBar>
